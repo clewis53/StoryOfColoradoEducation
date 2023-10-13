27359a0d4f5663c338df4528e457a9e52831402c
Caeley Lewis <caeleylewis53@gmail.com>
2023-10-13 14:28:43 -0600

    Add Files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Springboard Data Science Career Track Capstone 2',
+    author='Caeley Lewis',
+    license='',
+)
