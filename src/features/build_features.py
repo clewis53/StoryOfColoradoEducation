918575545e43efb963997e87ed96aacfa61fceaa
Caeley Lewis <caeleylewis53@gmail.com>
2023-10-13 19:13:52 -0600

    Complete LLM model
    The LLM model has been completed and implemented in the notebook

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pathlib import Path
-from src.features.preprocessors import KMeansPreprocessor
+from src.features.preprocessors import KMeansPreprocessor, LLMKMeansPreprocessor
 from src.input_output_functions import append_path
 
 
@@ -9,6 +9,7 @@ def main(input_filepath, output_filepath):
 
     build_kmeans(output_filepath, all_interim_df, high_interim_df)
 
+    build_llm_kmeans(output_filepath, all_interim_df, high_interim_df)
 
 def load_interim_data(input_filepath):
     """ Loads the interim data located at the input_filepath.
@@ -40,6 +41,33 @@ def build_kmeans(output_filepath, all_data=None, high_school=None, input_filepat
     preprocessor = KMeansPreprocessor(high_school=True)
     processed_high = preprocessor.fit_transform(high_school)
     processed_high.to_csv(append_path(output_filepath, 'kmeans_high_school.csv'), index=False)
+
+    # Returns the processed dataframes
+    return processed_all, processed_high
+
+
+def build_llm_kmeans(output_filepath, all_data=None, high_school=None, input_filepath=None):
+    """ Builds a dataframe from all_data and high_school that will be used for a llm processed kmeans model,
+    saves them, and returns them.
+    The preprocessor used is in preprocessors.LLCKMeansPreprocessor """
+
+    # Checks to see if all_data or high_school were not provided
+    if all_data is None or high_school is None:
+        # if they weren't makes sure that an input_filepath was provided
+        if input_filepath is None:
+            raise ValueError('If input_filepath is not provided, all_data and high_school must both be provided.')
+        # loads and replaces all_data, and high_school if necessary
+        all_data, high_school = load_interim_data(input_filepath)
+
+    # Processed the all_data and saves it
+    preprocessor = LLMKMeansPreprocessor()
+    processed_all = preprocessor.fit_transform(all_data)
+    processed_all.to_csv(append_path(output_filepath, 'llc_kmeans_all_data.csv'), index=False)
+
+    # Processes the high_school data and saves it
+    preprocessor = LLMKMeansPreprocessor(high_school=True)
+    processed_high = preprocessor.fit_transform(high_school)
+    processed_high.to_csv(append_path(output_filepath, 'llc_kmeans_high_school.csv'), index=False)
 
     # Returns the processed dataframes
     return processed_all, processed_high
--- src/features/preprocessors.py
+++ src/features/preprocessors.py
@@ -10,14 +10,7 @@ from sentence_transformers import SentenceTransformer
 class KMeansPreprocessor(BaseEstimator, TransformerMixin):
     INDEX_COLS = ['school_id', 'year']
 
-    CAT_COLS = ['pct_amind',
-                'pct_asian',
-                'pct_black',
-                'pct_hisp',
-                'pct_white',
-                'pct_2ormore',
-                'pct_fr',
-                'achievement_dir',
+    CAT_COLS = ['achievement_dir',
                 'growth_dir',
                 'overall_dir',
                 'school_grade']
@@ -117,7 +110,6 @@ class PassthroughTransformer(BaseEstimator, TransformerMixin):
 
 
 class FillBackForward(BaseEstimator, TransformerMixin):
-    NO_FILL = ['school', 'school_id', 'district_name', 'district_id', 'county']
 
     def fit(self, X, y=None):
         return self
@@ -177,22 +169,115 @@ class OutlierRemover(BaseEstimator, TransformerMixin):
         return X
 
 
-class LLC(BaseEstimator, TransformerMixin):
+class LLMKMeansPreprocessor(BaseEstimator, TransformerMixin):
+    INDEX_COLS = ['school_id', 'year']
+
+    COLS = ['achievement_dir',
+            'growth_dir',
+            'overall_dir',
+            'school_grade',
+            'est_total_pop',
+            'child_pov_ratio',
+            'child_adult_ratio',
+            'instruction_per_pupil',
+            'support_per_pupil',
+            'community_per_pupil',
+            'other_per_pupil',
+            'pct_amind',
+            'pct_asian',
+            'pct_black',
+            'pct_hisp',
+            'pct_white',
+            'pct_2ormore',
+            'pct_fr']
+
+    HIGH_COLS = ['eng_yn',
+                 'math_yn',
+                 'read_yn',
+                 'sci_yn',
+                 'pct_remediation',
+                 'graduation_rate']
+
+    def __init__(self, index_cols=None, cols=None, high_school=False, **kwargs):
+        # Set defaults if no information was provided
+        if index_cols is None:
+            self.index_cols = self.INDEX_COLS
+        else:
+            self.index_cols = index_cols
+
+        if cols is None:
+            if not high_school:
+                cols = self.COLS
+            else:
+                cols = self.COLS + self.HIGH_COLS
+        else:
+            cols = cols
+
+        na_filler = FillBackForward()
+        feature_builder = LLMKMeansFeatureBuilder(index_cols=self.index_cols, cols=cols)
+        outlier_remover = OutlierRemover(index_cols=self.index_cols)
+
+        steps = [('NA_filler', na_filler),
+                 ('feature_builder', feature_builder),
+                 ('outlier_remover', outlier_remover)]
+
+        self.pipeline = Pipeline(steps=steps)
+
+    def fit(self, X, y=None):
+        self.pipeline.fit(X)
+        return self
+
+    def transform(self, X):
+        X = self.pipeline.transform(X)
+        columns = self.index_cols + [str(i) for i in range(X.shape[1] - 2)]
+        return pd.DataFrame(X, columns=columns)
+
+
+class LLMKMeansFeatureBuilder(BaseEstimator, TransformerMixin):
+    """ A Transformer that builds all of the features for the model. """
+    VERBOSE_FEATURE_NAMES_OUT = False
+
+    def __init__(self, index_cols=None, cols=None, **kwargs):
+        llm = Pipeline(steps=[('encoder', LLM(cols))])
+        passthrough = Pipeline(steps=[('encoder', PassthroughTransformer())])
+        transformer = ColumnTransformer(transformers=[
+            ('', passthrough, index_cols),
+            ('num', llm, cols)],
+            verbose_feature_names_out=self.VERBOSE_FEATURE_NAMES_OUT)
+        self.pipeline = Pipeline(steps=[('transformer', transformer)])
+
+    def fit(self, X, y=None):
+        self.pipeline.fit(X)
+        return self
+
+    def transform(self, X):
+        return self.pipeline.transform(X)
+
+
+class LLM(BaseEstimator, TransformerMixin):
+
+    def __init__(self, cols, **kwargs):
+        self.cols = cols
 
     def fit(self, X, y=None):
         return self
 
-    def transform(self, X, cols):
-        sentences = X.apply(lambda x: self.compile_text(x), axis=1).tolist()
+    def transform(self, X):
+        """ Utilizes Sentence Embedding and BERT to convert all features to the numerical space """
+        sentences = X.apply(lambda x: self.compile_text(x, self.cols), axis=1).tolist()
 
+        # The location of the BERT Model
         model = SentenceTransformer(r"sentence-transformers/paraphrase-MiniLM-L6-v2")
+        # Process the sentences
         output = model.encode(sentences=sentences,
                               show_progress_bar=True,
                               normalize_embeddings=True)
 
+        # Create an embedding of the output and convert it to a DataFrame
         df_embedding = pd.DataFrame(output)
         return df_embedding
 
     @staticmethod
     def compile_text(X, cols):
+        """ Does a sentence embedding of all the features """
         return ',\n'.join([f'{col}: {X[col]}' for col in cols])
